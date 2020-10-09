a8c5485918bf59961d7a5cbf1be76ec71f20e154
perlancar (on pc-home) <perlancar@gmail.com>
2020-10-10 06:32:04 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/PERLANCAR/InfoFromCPANTesters.pm
@@ -0,0 +1,17 @@
+package Acme::CPANModules::PERLANCAR::InfoFromCPANTesters;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $LIST = {
+    summary => 'Distributions that gather information from CPANTesters',
+    entries => [
+        { module => "Acme-Test-crypt", summary => 'Check crypt() support in various platforms' },
+        { module => "App-PlatformInfo", summary => 'Result of Devel::Platform::Info on various testing machines' },
+    ],
+};
+
+1;
+# ABSTRACT:
